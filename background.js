8999ed911585057d8d945bdeb7d64292389d0561
ebowwa <81942069+ebowwa@users.noreply.github.com>
2023-01-02 03:19:57 -0800

    Update background.js

--- background.js
+++ background.js
@@ -1,13 +1,33 @@
+let jobTitle = 'Software Developer';
+let location = 'San Francisco, CA';
+let resume = 'resume.pdf';
+let coverLetter = 'cover_letter.pdf';
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-if (request.type === 'updateSettings') {
-const { jobTitle, location } = request.settings;
-chrome.storage.local.set({ settings: { jobTitle, location } });
-}
+  if (request.type === 'updateSettings') {
+    jobTitle = request.jobTitle;
+    location = request.location;
+    resume = request.resume;
+    coverLetter = request.coverLetter;
+  }
 });
 
-chrome.storage.local.get('settings', (result) => {
-const { jobTitle, location } = result.settings;
-chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-chrome.tabs.sendMessage(tabs[0].id, { type: 'searchAndApply', jobTitle, location });
-});
+async function searchAndApply() {
+  try {
+    // Use the LinkedIn API to search for jobs matching the specified job title and location
+    const searchResults = await searchJobs(jobTitle, location);
+
+    // Iterate through the search results and apply to each job
+    for (const job of searchResults) {
+      await applyToJob(job, resume, coverLetter);
+      await new Promise((resolve) => setTimeout(resolve, 1000)); // wait 1 second before applying to the next job
+    }
+  } catch (error) {
+    // Handle errors here
+    console.error(error);
+  }
+}
+
+chrome.browserAction.onClicked.addListener(() => {
+  searchAndApply();
 });
