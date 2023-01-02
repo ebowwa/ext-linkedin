20960dd68d4868b3ebd79073425964941c16e9b5
ebowwa <81942069+ebowwa@users.noreply.github.com>
2023-01-02 03:11:12 -0800

    Update background.js

--- background.js
+++ background.js
@@ -1,41 +1,13 @@
-// Intercept and modify the requests made by the extension to LinkedIn's website
-chrome.webRequest.onBeforeRequest.addListener(
-  function(details) {
-    // Modify the request here
-    return {requestHeaders: details.requestHeaders};
-  },
-  {urls: ["https://*.linkedin.com/*"]},
-  ["blocking"]
-);
-
-chrome.webRequest.onBeforeSendHeaders.addListener(
-  function(details) {
-    // Modify the request headers here
-    return {requestHeaders: details.requestHeaders};
-  },
-  {urls: ["https://*.linkedin.com/*"]},
-  ["blocking", "requestHeaders"]
-);
-
-// Use the LinkedIn API to search for jobs and apply
-function searchAndApply(jobTitle, location, resume, coverLetter) {
-  // Use the LinkedIn API to search for jobs matching the specified criteria
-  const jobs = searchJobs(jobTitle, location);
-
-  // Iterate through the list of jobs and apply to each one
-  jobs.forEach(job => applyToJob(job, resume, coverLetter));
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+if (request.type === 'updateSettings') {
+const { jobTitle, location } = request.settings;
+chrome.storage.local.set({ settings: { jobTitle, location } });
 }
+});
 
-// Search LinkedIn for jobs matching the specified criteria
-function searchJobs(jobTitle, location) {
-  // Use the LinkedIn API to search for jobs matching the specified criteria
-  const jobs = [];
-  // Return the list of jobs found
-  return jobs;
-}
-
-// Apply to the specified job using the provided resume and cover letter
-function applyToJob(job, resume, coverLetter) {
-  // Use the LinkedIn API to apply to the specified job using the provided resume and cover letter
-  // ...
-}
+chrome.storage.local.get('settings', (result) => {
+const { jobTitle, location } = result.settings;
+chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+chrome.tabs.sendMessage(tabs[0].id, { type: 'searchAndApply', jobTitle, location });
+});
+});
